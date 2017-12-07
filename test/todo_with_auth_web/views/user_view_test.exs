86eca931d8106c792bc2e6f3a6bc94d315a355e6
Felipe Skinner <felipeskinner@gmail.com>
2017-12-06 22:24:44 -0200

    Small tweaks

--- test/todo_with_auth_web/views/user_view_test.exs
+++ test/todo_with_auth_web/views/user_view_test.exs
@@ -6,16 +6,12 @@ defmodule TodoWithAuthWeb.UserViewTest do
     # Bring render/3 and render_to_string/3 for testing custom views
     import Phoenix.View
 
-    def user_json(user) do
-      %{
-        id: user.id,
-        email: user.email
-      }
+    defp user_json(user) do
+      %{id: user.id, email: user.email}
     end
 
     test "renders user.json" do
       user = insert(:user)
-      
       rendered_user = render(TodoWithAuthWeb.UserView, "user.json", user: user)
 
       assert rendered_user == user_json(user)
@@ -23,22 +19,16 @@ defmodule TodoWithAuthWeb.UserViewTest do
   
     test "renders index.json" do
       user = insert(:user)
-
       rendered_user = render(TodoWithAuthWeb.UserView, "index.json", %{users: [user]})
 
-      assert rendered_user == %{
-        data: [user_json(user)]
-      }
+      assert rendered_user == %{data: [user_json(user)]}
     end
 
     test "renders show.json" do
       user = insert(:user)
-
       rendered_user = render(TodoWithAuthWeb.UserView, "show.json", %{user: user})
 
-      assert rendered_user == %{
-        data: user_json(user)
-      }
+      assert rendered_user == %{data: user_json(user)}
     end
   end
   
