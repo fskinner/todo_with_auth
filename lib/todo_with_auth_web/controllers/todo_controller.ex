1591a423fa269973d10b8e2881beba061be6c7a3
Felipe Skinner <felipeskinner@gmail.com>
2018-01-14 23:35:05 -0200

    Refactor controller ifs

--- lib/todo_with_auth_web/controllers/todo_controller.ex
+++ lib/todo_with_auth_web/controllers/todo_controller.ex
@@ -27,10 +27,11 @@ defmodule TodoWithAuthWeb.TodoController do
   def show(conn, %{"id" => id}, current_user) do
     todo = Todos.get_todo!(id)
     
-    if todo.user_id == current_user.id do
+    with {:ok} <- check_ownership(todo, current_user) do
       render(conn, "show.json", todo: todo)
     else
-      conn |> unauthorized
+      _ ->
+        conn |> unauthorized
     end
   end
 
@@ -51,13 +52,13 @@ defmodule TodoWithAuthWeb.TodoController do
 
   def delete(conn, %{"id" => id}, current_user) do
     todo = Todos.get_todo!(id)
+    with {:ok} <- check_ownership(todo, current_user),
+        {:ok, %Todo{}} <- Todos.delete_todo(todo) do
 
-    if todo.user_id == current_user.id do
-      with {:ok, %Todo{}} <- Todos.delete_todo(todo) do
         send_resp(conn, :no_content, "")
-      end
     else
-      conn |> unauthorized
+      _ ->
+        conn |> unauthorized
     end
   end
 
@@ -65,5 +66,13 @@ defmodule TodoWithAuthWeb.TodoController do
     conn
     |> put_status(401)
     |> render(TodoWithAuthWeb.ErrorView, "401.json")
+  end
+
+  defp check_ownership(todo, current_user) do
+    if todo.user_id == current_user.id do
+      {:ok}
+    else
+      {:error}
+    end
   end
 end
